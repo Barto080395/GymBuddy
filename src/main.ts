5c9462d1e87923498c5df7ce17101bcc2b8acf34
Barto080395 <bartolomeobraccio95@live.com>
2026-03-18 14:47:19 +0100

    Added PWA support with manifest, icons and service worker

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { provideZoneChangeDetection } from "@angular/core";
+import { provideZoneChangeDetection, isDevMode } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(App, {
-  providers: [provideZoneChangeDetection(),provideRouter(routes)]
+  providers: [provideZoneChangeDetection(),provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 }).catch(err => console.error(err));
